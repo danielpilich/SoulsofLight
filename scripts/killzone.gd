7de6bedeffbbbb0e19e7fc3f84d22d8586d17040
Daniel Pilich <dpilich16@gmail.com>
2024-11-19 17:58:03 +0100

    Modify hero sprites and animations

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -5,10 +5,11 @@ extends Area2D
 
 func _on_body_entered(body):
 	if body.get_name() == "Player":
-		print("you died")
-		Engine.time_scale = 0.6
-		body.get_node("CollisionShape2D").queue_free()
-		timer.start()
+		print(body.health)
+		body.get_hit()
+		if body.health == 0:
+			Engine.time_scale = 0.4
+			timer.start()
 	
 func _on_timer_timeout():
 	Engine.time_scale = 1.0
--- scripts/player.gd
+++ scripts/player.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
 
+var health = 3
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
@@ -11,6 +12,9 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 @onready var weapon_animation_player: AnimationPlayer = $Weapon/AnimationPlayer
 @onready var weapon: Node2D = $Weapon
 
+func get_hit():
+	health -= 1
+	animated_sprite.play("hit")
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -54,15 +58,26 @@ func _physics_process(delta):
 			weapon.scale.x *= -1
 		
 	# Play animations
-	if is_on_floor():
-		if direction == 0:
-			animated_sprite.play("idle")
+	if health == 0:
+		set_physics_process(false)
+		animated_sprite.play("death")
+		await animated_sprite.animation_finished
+		animated_sprite.frame = 3
+		animated_sprite.pause()
+	else: 
+		if animated_sprite.animation == "hit":
+			set_physics_process(false)
+			await animated_sprite.animation_finished
+			set_physics_process(true)
+		if is_on_floor():
+			if direction == 0:
+				animated_sprite.play("idle")
+			else:
+				animated_sprite.play("run")
 		else:
-			animated_sprite.play("run")
-	else:
-		animated_sprite.play("jump")
+			animated_sprite.play("jump")
 
-	#Apply movement	
+	#Apply movement
 	if direction:
 		velocity.x = direction * SPEED
 	else:
