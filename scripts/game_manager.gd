7bd66e433a33421848ee6c83d046f777bd87799d
Daniel Pilich <dpilich16@gmail.com>
2025-01-10 20:27:29 +0100

    Fix user interface and step sounds

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -23,7 +23,6 @@ func _ready():
 
 func _process(delta):
 	enemies_left.text = "Enemies left: " + str(get_tree().get_nodes_in_group("Enemy").size())
-	next_wave.text = "Next wave in: %.f" % timer.time_left
 
 func _on_timer_timeout():
 	generate_enemies()
